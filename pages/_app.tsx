b950f68ebe86fefa766a9b381e6b487180d85102
pacharamai <pacharamai@hospital-os.com>
2021-10-20 22:52:28 +0700

    [fix]: basePath

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
       <meta httpEquiv='content-type' content='text/html; charset=utf-8' />
-      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <link rel='icon' type='image/x-icon' href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`} />
       <title>Profile Terminal</title>
     </Head>
     <Component {...pageProps} />
